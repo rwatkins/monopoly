c9cef4f3c94249c61495bc98a8d55c6fe7ab828b
Riley Watkins <riley@rileywatkins.com>
2011-09-06 17:51:31 -0700

    Clean up Chance function arguments

--- monopoly.py
+++ monopoly.py
@@ -8,7 +8,7 @@ class Board:
     available_hotels = 12
     available_houses = 32
     free_parking = 0
-    
+
     def __init__(self):
         # Properties around the board (every possible space to land on)
         self.properties = (
@@ -53,86 +53,87 @@ class Board:
             Property("Luxury Tax", "Special"),
             Property("Boardwalk", "Blue", 400),
         )
-        
+
         # Chance cards
         self.chance = [
             Chance(
                 "Advance to Go (Collect $200)",
-                advance_to_go()
+                advance_to_go
             ),
             Chance(
                 "Advance to Illinois Ave.",
-                advance_to_illinois_ave()
+                advance_to_illinois_ave
             ),
             Chance(
                 """Advance token to nearest Utility. If unowned, you may buy it
                 from the Bank. If owned, throw dice and pay owner a total of
                 ten times the amount thrown.""",
-                advance_to_nearest_utility()
+                advance_to_nearest_utility
             ),
             Chance(
                 "Bank pays you dividend of $50",
+                bank_pays_dividend
             ),
             Chance(
                 "Go back 3 spaces",
-                go_back()
+                go_back
             ),
             Chance(
                 "Go directly to Jail -- do not pass Go, do not collect $200",
-                go_directly_to_jail()
+                go_directly_to_jail
             ),
             Chance(
                 """Make general repairs on all your property - for each house
                 pay $25 - for each hotel $100""",
-                make_general_repairs()
+                make_general_repairs
             ),
             Chance(
                 "Pay poor tax of $15",
-                pay_poor_tax()
+                pay_poor_tax
             ),
             Chance(
                 """Take a trip to Reading Railroad -- if you pass Go collect
                 $200""",
-                advance_to_reading_railroad()
+                advance_to_reading_railroad
             ),
             Chance(
                 """Take a walk on the Boardwalk -- advance token to board
                 walk""",
-                advance_to_boardwalk()
+                advance_to_boardwalk
             ),
             Chance(
                 """You have been elected chairman of the board -- pay each
                 player $50""",
-                pay_each_player()
+                pay_each_player
             ),
             Chance(
                 "Your building loan matures -- collect $150",
-                building_loan()
+                building_loan
             ),
             Chance(
                 "You have won a crossword competition -- collect $100",
-                crossword_competition()
+                crossword_competition
             )
         ]
-        
+
         # Community Chest cards
         self.community_chest = []
 
 class Chance:
     """A card from the Chance deck."""
-    def __init__(self, text, function):
-        self.text = text
-        self.function = function
+    def __init__(self, description, func):
+        self.description = description
+        self.func = func
 
 class Property:
-    """A single square on the edge of the game board."""
+    """A single square on the game board."""
     def __init__(self, name, group, price=0, mandatory=False):
         self.name = name
         self.group = group
         self.price = price
         self.mandatory = mandatory
-        self.is_purchasable = not (self.group == "Special" or self.group == "Tax")
-        self.owner = Player # this will be a Player object
+        self.is_purchasable = self.group != "Special" and self.group != "Tax"
+        self.owner = None # this will be a Player object
         self.hotel = False
         self.houses = 0
 
@@ -140,85 +141,107 @@ class Player:
     def __init__(self, name, money=1500):
         self.name = name
         self.money = money
-        self.pos = pos # starting position on the board, from 0 to 39
-        self.status = "OK" # OK, In Jail, Loser, Winner
+        self.pos = 0 # starting position on the board, from 0 to 39
         self.doubles_counter = 0
         self.has_get_out_of_jail_free_card = False
-    
+
     def move_to(self, pos, pass_go=True):
         """
         Moves the Player to a specified square. If the Player passes Go, his
         money is increased by 200.
         """
+        if self.pos > pos and pass_go:
+            self.money += 200
         self.pos = pos
-        if self.pos > pos and pass_go: self.money += 200
-    
-    def set_status(self, status):
-        self.status = status
 
-def roll_dice(self, num_of_dice):
+    def status(self):
+        print self.name
+        print self.money
+        print self.pos
+
+def roll_dice(num_of_dice=1):
     """
     Returns a list of length num_of_dice, with each element containing the
     number that was rolled on that die.
     """
     sides = 6
     result = []
-    for n in range(1, num_of_dice + 1):
-        result.append(random.randrange(1, sides + 1))
+    for n in range(1, num_of_dice+1):
+        result.append(random.randrange(1, sides+1))
     return result
 
+#
 # Chance functions
-def advance_to_go(board, player, players):
+#
+def advance_to_go(**kwargs):
+    """Advance to Go (Collect $200)"""
+    assert "player" in kwargs.keys(), "'player' not found in arguments"
+    player = kwargs["player"]
     player.move_to(0)
     return
 
-def advance_to_illinois_ave(board, player, players):
+def advance_to_illinois_ave(**kwargs):
+    """Advance to Illinois Ave."""
+    assert "player" in kwargs.keys(), "'player' not found in arguments"
+    player = kwargs["player"]
     player.move_to(24)
     return
 
-def advance_to_nearest_utility(board, player, players):
+def advance_to_nearest_utility(**kwargs):
+    """
+    Advance token to nearest Utility. If unowned, you may buy it from the
+    Bank. If owned, throw dice and pay owner a total of ten times the amount
+    thrown.
+    """
+    assert "board" in kwargs.keys(), "'board' not found in arguments"
+    assert "player" in kwargs.keys(), "'player' not found in arguments"
+    board = kwargs["board"]
+    player = kwargs["player"]
     player.move_to(get_nearest("Utility", player.pos, board.properties))
-    if len(board.properties[player.pos].owner.name):
+    owner = board.properties[player.pos].owner
+    if owner:
+        print "Owner of", board.properties[player.pos].name, "is", owner.name
         dice = random.randrange(2, 13)
+        print player.name, "rolled", dice
+        print player.name, "pays", owner.name, "$", 10 * dice
         player.money -= 10 * dice
-        board.properties[player.pos].owner.money += 10 * dice
+        owner.money += 10 * dice
     else:
         # prompt for purchase
         pass
     return
 
-def advance_to_nearest_railroad(board, player, players):
-    player.move_to(get_nearest("Railroad", player.pos, board.properties))
-    if len(board.properties[player.pos].owner.name):
-        # pay owner twice the rent he/she is owed
-        pass
-    else:
-        # prompt for purchase
-        pass
-    return
-
-def advance_to_st_charles_place(board, player, players):
-    player.move_to(11)
-    return
-
-def bank_pays_dividend(board, player, players):
+def bank_pays_dividend(**kwargs):
+    """Bank pays you dividend of $50"""
+    assert "player" in kwargs.keys(), "'player' on found in arguments"
+    player = kwargs["player"]
     player.money += 50
     return
 
-def get_out_of_jail_free_card(board, player, players):
-    player.hasGetOutOfJailFreeCard = True
-    return
-
-def go_back(board, player, players):
+def go_back(**kwargs):
+    """Go back 3 spaces"""
+    assert "player" in kwargs.keys(), "'player' on found in arguments"
+    player = kwargs["player"]
     player.move_to(player.pos - 3, False)
     return
 
-def go_directly_to_jail(board, player, players):
+def go_directly_to_jail(**kwargs):
+    """Go directly to Jail -- do not pass Go, do not collect $200"""
+    assert "player" in kwargs.keys(), "'player' on found in arguments"
+    player = kwargs["player"]
     player.move_to(10, False)
     player.set_status("In Jail")
     return
 
-def make_general_repairs(board, player, players):
+def make_general_repairs(**kwargs):
+    """
+    Make general repairs on all your property - for each house pay $25 -
+    for each hotel $100
+    """
+    assert "board" in kwargs.keys(), "'board' not found in arguments"
+    assert "player" in kwargs.keys(), "'player' not found in arguments"
+    board = kwargs["board"]
+    player = kwargs["player"]
     cost = 0
     for x in player.properties:
         if x.hotel:
@@ -229,42 +252,79 @@ def make_general_repairs(board, player, players):
     board.freeParking += cost
     return
 
-def pay_poor_tax(board, player, players):
+def pay_poor_tax(**kwargs):
+    """Pay poor tax of $15"""
+    assert "board" in kwargs.keys(), "'board' not found in arguments"
+    assert "player" in kwargs.keys(), "'player' not found in arguments"
+    player, board = kwargs["player"], kwargs["board"]
     player.money -= 15
     board.freeParking += 15
     return
 
-def advance_to_reading_railroad(board, player, players):
+def advance_to_reading_railroad(**kwargs):
+    """Take a trip to Reading Railroad -- if you pass Go collect $200"""
+    assert "player" in kwargs.keys(), "'player' not found in arguments"
+    player = kwargs["player"]
     player.move_to(5)
     return
 
-def advance_to_boardwalk(board, player, players):
+def advance_to_boardwalk(**kwargs):
+    """Take a walk on the Boardwalk -- advance token to board walk"""
+    assert "player" in kwargs.keys(), "'player' not found in arguments"
+    player = kwargs["player"]
     player.move_to(39)
     return
 
-def pay_each_player(board, player, players):
+def pay_each_player(**kwargs):
+    """You have been elected chairman of the board -- pay each player $50"""
+    assert "players" in kwargs.keys(), "'players' not found in arguments"
+    assert "player" in kwargs.keys(), "'player' not found in arguments"
+    players, player = kwargs["players"], kwargs["player"]
     for p in players:
         if p.status != "Loser":
             player -= 50
             p += 50
     return
 
-def building_loan(board, player, players):
+def building_loan(**kwargs):
+    """Your building loan matures -- collect $150"""
+    assert "player" in kwargs.keys(), "'player' not found in arguments"
+    player = kwargs["player"]
     player.money += 150
     return
 
-def crossword_competition(board, player, players):
+def crossword_competition(**kwargs):
+    """You have won a crossword competition -- collect $100"""
+    assert "player" in kwargs.keys(), "'player' not found in arguments"
+    player = kwargs["player"]
     player.money += 100
     return
 
+#def advance_to_nearest_railroad(**kwargs):
+#    player.move_to(get_nearest("Railroad", player.pos, board.properties))
+#    if len(board.properties[player.pos].owner.name):
+#        # pay owner twice the rent he/she is owed
+#        pass
+#    else:
+#        # prompt for purchase
+#        pass
+#    return
+#
+#def advance_to_st_charles_place(**kwargs):
+#    player.move_to(11)
+#    return
+#
+#def get_out_of_jail_free_card(**kwargs):
+#    player.has_get_out_of_jail_free_card = True
+#    return
+
 def get_nearest(group, pos, properties):
-    '''Returns index of nearest Utility to the given position'''
+    """Returns index of nearest Utility to the given position"""
     # first look through all the properties from current position to the end
     # of the board
     for x in properties[pos:]:
         if x.group == group:
             return properties.index(x)
-    
     # then look through all the properties from the beginning of the board up
     # to the current position
     for x in properties[:pos]:
@@ -272,4 +332,4 @@ def get_nearest(group, pos, properties):
             return properties.index(x)
 
 if __name__ == '__main__':
-    print "Welcome to Monopoly!"
+    run()
