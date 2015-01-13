97b8ff0f821a2c338e44edb0e96e55e7f09bec3e
Riley Watkins <riley@rileywatkins.com>
2015-01-12 23:46:45 -0800

    Fix typo

--- monopoly.py
+++ monopoly.py
@@ -166,7 +166,7 @@ def run():
     print "Starting new game..."
 
     while True:
-        # End the game at round 1000
+        # End the game at round 10000
         if current_round >= 10000:
             game_over = True
             print "\nReached", current_round, "rounds. Game over."
